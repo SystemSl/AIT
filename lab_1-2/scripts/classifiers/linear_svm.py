99fbd0893427759f15f5a432364d70be3d76db14
SystemSL <Dmitriy.sulimov2003@gmail.com>
2026-03-30 20:06:42 +0400

    Исправление замечаний ЛР1

--- lab_1-2/scripts/classifiers/linear_svm.py
+++ lab_1-2/scripts/classifiers/linear_svm.py
@@ -36,8 +36,6 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
-                dW[:, j] += X[i]
-                dW[:, y[i]] -= X[i]
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -55,6 +53,17 @@ def svm_loss_naive(W, X, y, reg):
     # code above to compute the gradient.                                       #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    for i in range(num_train):
+        scores = X[i].dot(W)
+        correct_class_score = scores[y[i]]
+        for j in range(num_classes):
+            if j == y[i]:
+                continue
+            margin = scores[j] - correct_class_score + 1
+            if margin > 0:
+                dW[:, j] += X[i]
+                dW[:, y[i]] -= X[i]
 
     dW /= num_train
     dW += 2 * reg * W
